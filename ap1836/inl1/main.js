58cb400128e18a88b886624d978c55af8aebb454
daggelito02 <daggelito02@gmail.com>
2023-09-04 15:07:16 +0200

    Uppgift 3

---
+++ ap1836/inl1/main.js
@@ -0,0 +1,29 @@
+"use strict";
+/* Dag Fredriksson och da281a */
+/**
+ * Uppgift 1
+ * =========
+ */
+console.log(5 * 2 <= 12);
+console.log(55 > 22);
+console.log(16 / 4 == 4);
+console.log(8 + 2 < 128);
+console.log(32 * 8 != 255);
+/**
+ * Uppgift 2
+ * =========
+ */
+const tuesday = "Tisdag";
+const hamburger = "Hamburgare";
+const beBack = "I'll be back";
+console.log(tuesday.substring(0, 3));
+console.log(hamburger.substring(3));
+console.log(beBack.substring(5));
+/**
+ * Uppgift 3
+ * =========
+ */
+const learning = "It's Learning".substring(5).toUpperCase();
+const goodParts = "JavaScript: The Good Parts".substring(16).toLowerCase();
+console.log(learning);
+console.log(goodParts);
