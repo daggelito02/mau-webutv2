79d3b169a92c88ffdd7ace2365dc46d26f1346c6
daggelito02 <daggelito02@gmail.com>
2023-09-05 18:09:23 +0200

    Uppgift 4

--- ap1836/inl1/main.js
+++ ap1836/inl1/main.js
@@ -19,28 +19,34 @@ console.log(55 > 22);
 console.log(16 / 4 == 4);
 console.log(8 + 2 < 128);
 console.log(32 * 8 != 255);
+
 /**
  * Uppgift 2
  * =========
  */
+
 const tuesday = "Tisdag";
 const hamburger = "Hamburgare";
 const beBack = "I'll be back";
 console.log(tuesday.substring(0, 3));
 console.log(hamburger.substring(3));
 console.log(beBack.substring(5));
+
 /**
  * Uppgift 3
  * =========
  */
+
 const learning = "It's Learning".substring(5).toUpperCase();
 const goodParts = "JavaScript: The Good Parts".substring(16).toLowerCase();
 console.log(learning);
 console.log(goodParts);
+
 /**
  * Uppgift 4
  * =========
  */
+
 var numbers = [128, 256, 512, 1024, 2048];
 let sumOfNumbers = 0;
 
@@ -48,8 +54,9 @@ numbers.forEach( num => {
     sumOfNumbers += num;
 })
 
+console.log(sumOfNumbers); //Uppgift 4.1
 const avgNumber = sumOfNumbers / numbers.length;
-console.log(avgNumber); 
+console.log(avgNumber); //Uppgift 4.2
 
 var numbers2 = [128, 256, 512, 1024, 2048, 32];
 sumOfNumbers = 0;
@@ -58,5 +65,9 @@ numbers2.forEach( num => {
     sumOfNumbers += num;
 })
 
-console.log(sumOfNumbers);
+console.log(sumOfNumbers); //Uppgift 4.3
 
+/**
+ * Uppgift 5
+ * =========
+ */
