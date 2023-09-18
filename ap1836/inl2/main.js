dc111f2b051a92f92bfca5c4cdf3bb40d61a9636
daggelito02 <daggelito02@gmail.com>
2023-09-18 11:22:25 +0200

    Inlämningsuppgift 2 Uppgift 1

--- ap1836/inl2/main.js
+++ ap1836/inl2/main.js
@@ -5,3 +5,22 @@
  * Uppgift 1
  * =========
  */
+
+const max = function(number1, number2) {
+    let result = 0;
+    if (number1 >= number2) {
+        result = number1;
+    } else if (number1 < number2) {
+        result = number2;
+    }
+    return result;
+  };
+  const min = function(number1, number2) {
+    let result = 0;
+    if (number1 <= number2) {
+        result = number1;
+    } else if (number1 > number2) {
+        result = number2;
+    }
+    return result;
+  };
